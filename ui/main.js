62dffc53f99981842a099f8f62081f81a37925de
jackofalltradesprogrammer <jackofalltradesprogrammer@gmail.com>
2018-02-23 22:28:06 -0500

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,12 +2,23 @@
 var button = document.getElementById('counter');
 button.onclick = function () {
     
-    // Make a request to the counter endpoint
+    // Create a request
+    var request = new XMLHttpRequest();
     
     //Capture the response and store it in a variable
+    request.onreadystatechange = function() {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            // Take some action
+            if (request.status === 200){
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+        
+    };
     
-    // Render the variable in the correct span
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
+    // Make the request
+    request.open('GET', 'http://http://jackofalltradesprogrammer.imad.hasura-app.io/counter', true);
+    request.send(null);
 };
