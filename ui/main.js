a1fa4b40249ff8bc354ac4fbd1b4dbe23686e2b2
jackofalltradesprogrammer <jackofalltradesprogrammer@gmail.com>
2018-02-23 21:05:16 -0500

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'New value';
 
 // Move the image
-var img = docment.getElementById('madi');
+var img = document.getElementById('madi');
 img.onclick = function() {
   img.style.marginLeft = '100px';  
 };
