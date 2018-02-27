f25c9149e2266754d9a5c50b0762d04bb7219ec1
jackofalltradesprogrammer <jackofalltradesprogrammer@gmail.com>
2018-02-27 12:22:14 -0500

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,8 +24,7 @@ button.onclick = function () {
 };
 
 //submit name
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
     // Make a reuest to the server and send the name
@@ -52,6 +51,8 @@ submit.onclick = function () {
     };
     
     // Make the request - localhost machine cannot access the counter from the url - no authentication
+    var nameInput = document.getElementById('name');
+    var name = nameInput.value;
     request.open('GET', 'http://jackofalltradesprogrammer.imad.hasura-app.io/submit-name?name=' + name, true);
     request.send(null);
     // Capture a list of names and render it as a list
