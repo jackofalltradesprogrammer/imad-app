8be74489be1117d2698245a59f70b06bf1ee7d3b
jackofalltradesprogrammer <jackofalltradesprogrammer@gmail.com>
2018-02-28 14:17:35 -0500

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -133,7 +133,7 @@ app.get('/articles/:articleName', function (req, res) {
                 if (result.rows.length === 0){
                     res.status(404).send('Article not found');
                 }else {
-                    console.log("ahppy");
+                    res.status(200).send('Article fuck you');
                     // var articleData = result.rows[0];
                     // res.send(createTemplate(articleData()));
                 }
